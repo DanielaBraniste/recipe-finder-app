298ac4b0c8cb81764802fe914fadc4d096336557
DanielaBraniste <43215641+DanielaBraniste@users.noreply.github.com>
2025-10-02 23:51:26 +0300

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-restricted-globals */
+
+// This service worker can be customized!
+// See https://developers.google.com/web/tools/workbox/modules
+// for the list of available Workbox modules, or add any other
+// code you'd like.
+
+const CACHE_NAME = 'kitchen-planner-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/static/css/main.css',
+  '/static/js/main.js',
+  '/manifest.json',
+  '/recipes.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
