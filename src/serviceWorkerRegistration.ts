fe2b4be94ac1e80c26be7b0e9a854770cf81fab1
DanielaBraniste <43215641+DanielaBraniste@users.noreply.github.com>
2025-10-02 23:54:21 +0300

    Update serviceWorkerRegistration.ts

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -1,3 +1,6 @@
+// This optional code is used to register a service worker.
+// register() is not called by default.
+
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     window.location.hostname === '[::1]' ||
@@ -11,13 +14,13 @@ type Config = {
 
 export function register(config?: Config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    const publicUrl = new URL(process.env.PUBLIC_URL || '', window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       return;
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL || ''}/service-worker.js`;
 
       if (isLocalhost) {
         checkValidServiceWorker(swUrl, config);
